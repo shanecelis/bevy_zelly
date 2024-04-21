86163fb4cd80c28e53295b3b63a12c9dd0feaea9
Shane Celis <shane.celis@gmail.com>
2024-04-21 16:27:22 -0400

    feature: Add level1.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 use bevy::prelude::*;
 use bevy_ecs_ldtk::prelude::*;
 
+#[derive(Default, Component)]
+struct Player;
+
+#[derive(Default, Bundle, LdtkEntity)]
+struct PlayerBundle {
+    player: Player,
+    #[sprite_bundle]
+    sprite_bundle: SpriteBundle,
+    #[grid_coords]
+    grid_coords: GridCoords,
+}
+
 fn main() {
     App::new()
         .add_plugins(
@@ -8,20 +20,57 @@ fn main() {
         )
         .add_plugins(LdtkPlugin)
         .add_systems(Startup, setup)
+        .add_systems(Update,
+                     (move_player_from_input,
+                     translate_grid_coords_entities))
         .insert_resource(LevelSelection::index(0))
-        .register_ldtk_entity::<MyBundle>("MyEntityIdentifier")
+        //.register_ldtk_entity::<MyBundle>("MyEntityIdentifier")
+        .register_ldtk_entity::<PlayerBundle>("Player")
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
-
+    let mut camera = Camera2dBundle::default();
+    camera.projection.scale = 0.5;
+    commands.spawn(camera);
     commands.spawn(LdtkWorldBundle {
-        ldtk_handle: asset_server.load("level0.ldtk"),
+        ldtk_handle: asset_server.load("level1.ldtk"),
         ..Default::default()
     });
 }
 
+fn move_player_from_input(
+    mut players: Query<&mut GridCoords, With<Player>>,
+    input: Res<ButtonInput<KeyCode>>,
+) {
+    let movement_direction = if input.just_pressed(KeyCode::KeyW) {
+        GridCoords::new(0, 1)
+    } else if input.just_pressed(KeyCode::KeyA) {
+        GridCoords::new(-1, 0)
+    } else if input.just_pressed(KeyCode::KeyS) {
+        GridCoords::new(0, -1)
+    } else if input.just_pressed(KeyCode::KeyD) {
+        GridCoords::new(1, 0)
+    } else {
+        return;
+    };
+
+    for mut player_grid_coords in players.iter_mut() {
+        let destination = *player_grid_coords + movement_direction;
+        *player_grid_coords = destination;
+    }
+}
+const GRID_SIZE: i32 = 16;
+
+fn translate_grid_coords_entities(
+    mut grid_coords_entities: Query<(&mut Transform, &GridCoords), Changed<GridCoords>>,
+) {
+    for (mut transform, grid_coords) in grid_coords_entities.iter_mut() {
+        transform.translation =
+            bevy_ecs_ldtk::utils::grid_coords_to_translation(*grid_coords, IVec2::splat(GRID_SIZE))
+                .extend(transform.translation.z);
+    }
+}
 #[derive(Default, Component)]
 struct ComponentA;
 
